162fc36556ad467b488b6a4d3cf8069dcd26ae7c
nafij-islam <sahariannafis70@gmail.com>
2025-11-18 09:25:14 +0600

    add resume

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -10,59 +10,69 @@ import { TypeAnimation } from 'react-type-animation';
 
 const Banner = () => {
   return (
-   <section className='bg-[#bddadf] py-[70px] lg:py-[100px]  px-2'>
-       <div className='max-w-[1320px] mx-auto '>
+   <section className='bg-[#bddadf] py-[70px] lg:py-[100px] px-2'>
+       <div className='max-w-[1320px] mx-auto'>
            <div>
-               {/* 2. Replace the static h1 with the TypeAnimation component */}
+               {/* Animated Heading */}
                <TypeAnimation
-                  // The sequence of strings to type
                   sequence={[
-                    "HEY,I'M NAFIJ ISLAM", // Types this string
-                    1500, // Waits 1.5 seconds
-                    "I'M A FRONTEND-FOCUSED WEB DEVELOPER", // Deletes and types this string
-                    1500, // Waits 1.5 seconds
+                    "HEY,I'M NAFIJ ISLAM",
+                    1500,
+                    "I'M A FRONTEND-FOCUSED WEB DEVELOPER",
+                    1500,
                   ]}
-                  // Use 'h1' as the wrapper element
                   wrapper="h1"
-                  // Typing speed
                   speed={30}
-                  // Apply all your original styles
                   className='text-center text-[#318c77] lg:w-[900px] mx-auto text-[35px] lg:text-[50px] font-san font-bold'
-                  // Repeat the animation forever
                   repeat={Infinity}
-                  // Show the typing cursor
                   cursor={true}
                 />
-               
-               <p className='text-center md:text-[18px] lg:text-[20px] md:w-[600px] pb-10 font-dms  pt-5 lg:w-[900px] mx-auto'>I am a Frontend-focused Web Developer, building responsive and user-friendly websites and web applications that improve user experience and contribute to the overall success of the product.</p>
-                <div className='text-center'>
 
-               <button className='lg:py-4 py-4 rounded-[5px] hover:text-[#1E7EA7] duration-300  hover:bg-transparent border-transparent hover:border-[#1E7EA7] border-2 hover:border-2  lg:px-7 px-5   bg-[#1E7EA7] text-white font-bold font-san'>View My Project</button>
-                </div>
+               <p className='text-center md:text-[18px] lg:text-[20px] md:w-[600px] pb-10 font-dms pt-5 lg:w-[900px] mx-auto'>
+                  I am a Frontend-focused Web Developer, building responsive and user-friendly websites and web applications that improve user experience and contribute to the overall success of the product.
+               </p>
+
+               {/* Buttons */}
+               <div className='text-center flex justify-center gap-4'>
+
+                 {/* View Projects Button */}
+                 <button className='lg:py-4 py-4 rounded-[5px] hover:text-[#1E7EA7] duration-300 hover:bg-transparent border-transparent hover:border-[#1E7EA7] border-2 lg:px-7 px-5 bg-[#1E7EA7] text-white font-bold font-san'>
+                   View My Project
+                 </button>
+
+                 {/* Resume Button */}
+                 <a href="../../../public/Nafij-Islam-Resume.pdf" target="_blank">
+                   <button className='lg:py-4 py-4 rounded-[5px] hover:text-[#318c77] duration-300 hover:bg-transparent border-[#1E7EA7] border-2 lg:px-7 px-5 bg-[#1E7EA7] text-white font-bold font-san'>
+                     Download Resume
+                   </button>
+                 </a>
+
+               </div>
            </div>
        </div>
+
+       {/* Social Icons */}
        <div className='text-[30px] sm:hidden md:hidden hidden text-[#1E7EA7] top-70 left-2 absolute lg:flex flex-col gap-4'>
         <a className='hover:text-[#cf0bed] duration-200' target="_blank" href="https://www.linkedin.com/in/nafij-islam-b15261357/">
-
-        <FaLinkedin />
+          <FaLinkedin />
         </a>
+
         <a className='hover:text-[#cf0bed] duration-200' target="_blank" href="https://github.com/nafij-islam">
-
-        <FaSquareGithub />
+          <FaSquareGithub />
         </a>
+
         <a className='hover:text-[#cf0bed] duration-200' target="_blank" href="https://www.facebook.com/saharian.nafis.256620">
-
-        <FaSquareFacebook />
+          <FaSquareFacebook />
         </a>
+
         <a className='hover:text-[#cf0bed] duration-200'>
-
-        <FaWhatsappSquare />
+          <FaWhatsappSquare />
         </a>
-        <a className='hover:text-[#cf0bed] duration-200' href="">
 
-        <FaTelegram />
+        <a className='hover:text-[#cf0bed] duration-200' href="#">
+          <FaTelegram />
         </a>
-        </div>
+       </div>
    </section>
   )
 }
